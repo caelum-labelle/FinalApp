4e768a2389474a47681f5c130fb85d5f315ea955
Kyla <kylaguidomanatad@gmail.com>
2023-05-22 21:16:25 +0800

    oks na

--- app/src/main/java/com/example/finalapp/Explore2.java
+++ app/src/main/java/com/example/finalapp/Explore2.java
@@ -24,32 +24,8 @@ public class Explore2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityExplore2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.f);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_explore2);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.f.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void setSupportActionBar(MaterialCardView f) {
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_explore2);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/finalapp/ProfileFragment.java
+++ app/src/main/java/com/example/finalapp/ProfileFragment.java
@@ -1,21 +1,53 @@
 package com.example.finalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
 
+    private Button logoutButton;
 
-    @Override
+    /*Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }*/
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        logoutButton = view.findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle logout button click event
+                logout();
+            }
+        });
+
+        return view;
+    }
+
+    private void logout() {
+        // Perform any necessary logout actions
+
+        // Navigate to Home.java activity
+        Intent intent = new Intent(getActivity(), Login.class);
+        startActivity(intent);
+        getActivity().finish(); // Optional: Close the current activity (ProfileFragment) after logout
     }
 
 
